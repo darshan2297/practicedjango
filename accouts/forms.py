e6bba2a6a7edf4f902175993ea1dd0ebda42f184
darshan2297 <darshanvankawala2297@gmail.com>
2020-01-29 09:44:34 +0530

    mychanges

--- accouts/forms.py
+++ accouts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.utils import timezone
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 class authform(forms.Form):
     Username = forms.CharField()
@@ -7,4 +9,15 @@ class authform(forms.Form):
     First_name = forms.CharField()
     Last_name = forms.CharField()
     Email_address=forms.EmailField()
+    botcatcher = forms.CharField(required=False,widget=forms.HiddenInput)
     # Date_joined=forms.DateTimeField(default=timezone.now())
+    
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        
+        if(len(botcatcher) > 0):
+            raise forms.ValidationError("GotchA Bot")
+            return botcatcher
+            
+           
+        
